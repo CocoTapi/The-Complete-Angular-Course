e14e77b4048e69c710f2d373a5749fc3418a7daa
CocoTapi <shioriavino@gmail.com>
2024-08-11 17:16:24 -0400

    setup initial project, add component

---
+++ Angular-Fundamentals/old-todo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { NewCourseComponent } from './new-course/new-course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    NewCourseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular-Fundamentals/old-todo/src/app/new-course/new-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-course',
+  templateUrl: './new-course.component.html',
+  styleUrl: './new-course.component.scss'
+})
+export class NewCourseComponent {
+
+}

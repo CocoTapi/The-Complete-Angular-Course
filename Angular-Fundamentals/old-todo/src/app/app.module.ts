05879c581e21852ee9c0d47b63789175029c34c2
CocoTapi <shioriavino@gmail.com>
2024-08-11 17:30:40 -0400

    add service

--- Angular-Fundamentals/old-todo/src/app/app.module.ts
+++ Angular-Fundamentals/old-todo/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { NewCourseComponent } from './new-course/new-course.component';
+import { CoursesService } from './courses.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { NewCourseComponent } from './new-course/new-course.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Angular-Fundamentals/old-todo/src/app/courses.service.ts
@@ -0,0 +1,5 @@
+export class CoursesService {
+    getCourses(){
+        return ["Course1", "course2", "course3"]
+    }
+}

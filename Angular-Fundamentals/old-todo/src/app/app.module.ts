4bdc1877903a7db3afa8cd808ae1e134eef170e1
CocoTapi <shioriavino@gmail.com>
2024-08-11 19:22:14 -0400

    add basic rules for component, pipe, styling

--- Angular-Fundamentals/old-todo/src/app/app.module.ts
+++ Angular-Fundamentals/old-todo/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { NewCourseComponent } from './new-course/new-course.component';
 import { CoursesService } from './courses.service';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    NewCourseComponent
+    NewCourseComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     CoursesService

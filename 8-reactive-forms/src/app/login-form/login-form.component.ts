9e2d2290b8001fe997325f66b27476d328980f5d
CocoTapi <shioriavino@gmail.com>
2024-09-06 21:51:34 -0400

    add form module

--- 8-reactive-forms/src/app/login-form/login-form.component.ts
+++ 8-reactive-forms/src/app/login-form/login-form.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'login-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
-
+  form = new FormGroup({
+    'username': new FormControl(),
+    'password': new FormControl(),
+  })
 }

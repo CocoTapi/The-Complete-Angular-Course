acecacdafda3f5ff4b0eb1827a560cb761b23a05
CocoTapi <shioriavino@gmail.com>
2025-01-03 20:38:19 -0500

    add built in validators for username

--- 8-reactive-forms/src/app/login-form/login-form.component.ts
+++ 8-reactive-forms/src/app/login-form/login-form.component.ts
@@ -1,16 +1,25 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
+
 export class LoginFormComponent {
-  form = new FormGroup({
-    'username': new FormControl(),
-    'password': new FormControl(),
-  })
+  loginForm = new FormGroup({
+    'username': new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    'password': new FormControl('', Validators.required),
+  });
+
+  get username(){
+    return this.loginForm.get('username') as FormControl | null;
+  }
 }
